778f723b0d4304fbbbb52f78bd3a7f72206459e9
Grigory Titov <titovgrisha04@gmail.com>
2023-09-22 14:11:27 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsC#_University/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsC__University
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //подписался на событие Paint
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            /*
+            string str = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⣄⢘⣒⣀⣀⣀⣀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣽⣿⣛⠛⢛⣿⣿⡿⠟⠂⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⣤⣾⣿⣿⣿⣿⣿⣿⣿⣷⣿⡆⠀\r\n⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠀\r\n⠀⠀⠀⢀⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀\r\n⠀⠀⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠜⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⢿⣿⣿⣿⣿⠿⠿⣿⣿⡿⢿⣿⣿⠈⣿⣿⣿⡏⣠⡴⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⣠⣿⣿⣿⡿⢁⣴⣶⣄⠀⠀⠉⠉⠉⠀⢻⣿⡿⢰⣿⡇⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⢿⣿⠟⠋⠀⠈⠛⣿⣿⠀⠀⠀⠀⠀⠀⠸⣿⡇⢸⣿⡇⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⢸⣿⠀⠀⠀⠀⠀⠘⠿⠆⠀⠀⠀⠀⠀⠀⣿⡇⠀⠿⠇⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+            int scale = 10;*/
+            //Амонг ас чел
+            
+             string str = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣤⣤⣤⣤⣤⣶⣦⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡿⠛⠉⠙⠛⠛⠛⠛⠻⢿⣿⣷⣤⡀⠀⠀⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⠋⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⠈⢻⣿⣿⡄⠀⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⣸⣿⡏⠀⠀⠀⣠⣶⣾⣿⣿⣿⠿⠿⠿⢿⣿⣿⣿⣄⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠁⠀⠀⢰⣿⣿⣯⠁⠀⠀⠀⠀⠀⠀⠀⠈⠙⢿⣷⡄⠀ \r\n⠀⠀⣀⣤⣴⣶⣶⣿⡟⠀⠀⠀⢸⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⠀ \r\n⠀⢰⣿⡟⠋⠉⣹⣿⡇⠀⠀⠀⠘⣿⣿⣿⣿⣷⣦⣤⣤⣤⣶⣶⣶⣶⣿⣿⣿⠀ \r\n⠀⢸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠀ \r\n⠀⣸⣿⡇⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠉⠻⠿⣿⣿⣿⣿⡿⠿⠿⠛⢻⣿⡇⠀⠀ \r\n⠀⣿⣿⠁⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣧⠀⠀ \r\n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \r\n⠀⣿⣿⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀ \r\n⠀⢿⣿⡆⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡇⠀⠀ \r\n⠀⠸⣿⣧⡀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⠃⠀⠀ \r\n⠀⠀⠛⢿⣿⣿⣿⣿⣇⠀⠀⠀⠀⠀⣰⣿⣿⣷⣶⣶⣶⣶⠶⠀⢠⣿⣿⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⣽⣿⡏⠁⠀⠀⢸⣿⡇⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⣿⣿⠀⠀⠀⠀⠀⣿⣿⡇⠀⢹⣿⡆⠀⠀⠀⣸⣿⠇⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⢿⣿⣦⣄⣀⣠⣴⣿⣿⠁⠀⠈⠻⣿⣿⣿⣿⡿⠏⠀⠀⠀⠀ \r\n⠀⠀⠀⠀⠀⠀⠀⠈⠛⠻⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀";
+            int scale = 10;
+            
+            int x=1,y=1;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ( str[i] == '\n') { y++;x = 0; }      
+                else x++;
+            }
+            this.Size = new Size(x * (scale+2), y * (scale+2));//изменили размер холста
+            
+            // инициализация холста(поверхности)
+            Graphics g = Graphics.FromHwnd(this.Handle);
+            g.Clear(Color.Black);
+
+            x = -1;y = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == '\n') { y++; x = -1; }
+                else x++;
+                if (str[i] != '\n' && str[i] != '⠀' && str[i] != '\t' && str[i] != '\r')
+                {
+                    switch (str[i])
+                    {
+                        case '⢀': g.FillRectangle(new SolidBrush(Color.FromArgb(32, 32, 32)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⣠': g.FillRectangle(new SolidBrush(Color.FromArgb(64, 64, 64)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⣤': g.FillRectangle(new SolidBrush(Color.FromArgb(128, 128, 128)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⣀': g.FillRectangle(new SolidBrush(Color.FromArgb(64, 64, 64)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⣴': g.FillRectangle(new SolidBrush(Color.FromArgb(128, 128, 128)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⠟': g.FillRectangle(new SolidBrush(Color.FromArgb(128, 128, 128)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⠉': g.FillRectangle(new SolidBrush(Color.FromArgb(64, 64, 64)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⢸': g.FillRectangle(new SolidBrush(Color.FromArgb(32, 32, 32)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⣿': g.FillRectangle(new SolidBrush(Color.FromArgb(228, 228, 228)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⠿': g.FillRectangle(new SolidBrush(Color.FromArgb(228, 228, 228)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⠛': g.FillRectangle(new SolidBrush(Color.FromArgb(64, 64, 64)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        case '⢦': g.FillRectangle(new SolidBrush(Color.FromArgb(16, 16, 16)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                        default: g.FillRectangle(new SolidBrush(Color.FromArgb(128, 128, 128)), new Rectangle(x * scale, y * scale, scale, scale)); break;
+                    }
+                }
+            }
+
+
+        }
+    }
+}
